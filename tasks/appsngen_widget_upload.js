a77eb799c994c5a75a702a749907d724978a771f
Alexander Bukley <alexander_bukley@epam.com>
2016-05-24 17:35:30 +0300

    Simplify code

--- tasks/appsngen_widget_upload.js
+++ tasks/appsngen_widget_upload.js
@@ -10,39 +10,6 @@ var npmOpen = require('open');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 /////////////////////////////////
-// local functions
-////////////////////////////////
-
-var encodeToBase64 = function (input) {
-
-    var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-    var output = '', chr1, chr2, chr3, enc1, enc2, enc3, enc4, i = 0;
-
-    while (i < input.length) {
-        chr1 = input.charCodeAt(i++);
-        chr2 = input.charCodeAt(i++);
-        chr3 = input.charCodeAt(i++);
-
-        enc1 = chr1 >> 2;
-        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-        enc4 = chr3 & 63;
-
-        if (isNaN(chr2)) {
-            enc3 = enc4 = 64;
-        } else if (isNaN(chr3)) {
-            enc4 = 64;
-        }
-
-        output += keyStr.charAt(enc1);
-        output += keyStr.charAt(enc2);
-        output += keyStr.charAt(enc3);
-        output += keyStr.charAt(enc4);
-    }
-    return output;
-};
-
-/////////////////////////////////
 // Task register
 ////////////////////////////////
 module.exports = function (grunt) {
@@ -75,7 +42,7 @@ module.exports = function (grunt) {
                         json: true,
                         headers: {
                             'Content-Type': 'application/json',
-                            'Authorization': 'Basic: ' + encodeToBase64(credentials)
+                            'Authorization': 'Basic: ' + new Buffer(credentials).toString('base64')
                         }
                     },
                     function (error, response, body) {
