86d19fb940a8d38e392198a35c0e88994e7c63cd
Alexander Bukley <alexander_bukley@epam.com>
2016-06-14 18:55:45 +0300

    Update upload processing

--- tasks/appsngen_widget_upload.js
+++ tasks/appsngen_widget_upload.js
@@ -3,10 +3,10 @@
 /////////////////////////////////
 // Required
 ////////////////////////////////
-var request = require('request');
-var fs = require('fs');
-var waterfall = require('async-waterfall');
+var upload = require('appsngen-widget-upload');
 var npmOpen = require('open');
+var Promise = require('bluebird').Promise;
+var post = Promise.promisify(require('request').post);
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 /////////////////////////////////
@@ -14,122 +14,47 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 ////////////////////////////////
 module.exports = function (grunt) {
     grunt.registerTask('appsngen_widget_upload', 'AppsNgen task for uploading widgets', function () {
-
-        this.async();
+        var done = this.async();
         var gruntOptions = this.options();
-
-        // variables block
-        var username = gruntOptions.username;
-        var password = gruntOptions.password;
-        var serviceAddress = gruntOptions.serviceAddress;
-        var zipFilePath = gruntOptions.zipFilePath;
-        var replaceIfExists = gruntOptions.replaceIfExists;
-        var openInBrowserAfterUpload = gruntOptions.openInBrowserAfterUpload;
-
-        var credentials = username + ':' + password;
+        var credentials = gruntOptions.username + ':' + gruntOptions.password;
+        var uploadOptions = {
+            replaceIfExists: gruntOptions.replaceIfExists,
+            serviceAddress: gruntOptions.serviceAddress,
+            zipFilePath: gruntOptions.zipFilePath
+        };
 
         // Processing block
-        waterfall([
-            function (callback) { //token request
-                var options = {};
-
-                request.post(
-                    serviceAddress + '/rest-services/tokens/identity',
-                    {
-                        body: {
-                            scope: {}
-                        },
-                        json: true,
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': 'Basic: ' + new Buffer(credentials).toString('base64')
-                        }
-                    },
-                    function (error, response, body) {
-                        if (error) {
-                            throw error;
-                        }
-                        if (!error && response.statusCode === 201) {
-                            console.log('Get token success!');
-                            options.token = body.identityToken;
-                            callback(null, options);
-                        }else{
-                            console.log(body);
-                            throw ('Unexpected response: ' + response.statusCode);
-                        }
-                    }
-                );
-            },
-            function (options, callback) { // read zip file
-                fs.readFile(zipFilePath, 'binary', function (err, data) {
-                    if (err) {
-                        throw err;
-                    }
-                    options.zipFile = new Buffer(data, 'binary');
-                    console.log('File read success!');
-                    callback(null, options);
-                });
-            },
-            function (options, callback) { //upload
-                request.post(
-                    serviceAddress + '/viewer/widgets',
-                    {
-                        body: options.zipFile,
-                        headers: {
-                            'Content-Type': 'application/zip',
-                            'Authorization': 'Bearer ' + options.token
-                        }
-                    },
-                    function (error, response, body) {
-                        if (error) {
-                            throw error;
-                        }
-                        else {
-                            console.log(body);
-                            options.statusCode = response.statusCode;
-                            options.urn = JSON.parse(response.body).urn;
-                            callback(null, options);
-                        }
-                    }
-                );
-            },
-            function (options, callback) { //update if already exists
-                if (options.statusCode === 409) {
-                    if (!replaceIfExists) {
-                        console.log('To replace existing widget, set replaceIfExists option to \'true\'');
-                        return;
-                    }
-                    console.log('Post upload conflict, trying to update existing widget...');
-                    request.put(
-                        serviceAddress + '/viewer/widgets',
-                        {
-                            body: options.zipFile,
-                            headers: {
-                                'Content-Type': 'application/zip',
-                                'Authorization': 'Bearer ' + options.token
-                            }
-                        },
-                        function (error, response) {
-                            if (error) {
-                                throw error;
-                            }
-                            else {
-                                console.log(response.body);
-                                console.log('Upload success!');
-                                options.urn = JSON.parse(response.body).urn;
-                                callback(null, options);
-                            }
-                        }
-                    );
-                } else {
-                    console.log('Upload success!');
-                    callback(null, options);
+        post(gruntOptions.serviceAddress + '/rest-services/tokens/identity', {
+                body: {
+                    scope: {}
+                },
+                json: true,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Basic: ' + new Buffer(credentials).toString('base64')
                 }
-            },
-            function (options) { // open in browser
-                if (openInBrowserAfterUpload) {
-                    npmOpen(serviceAddress + '/product/marketplace/widgets/config/' + options.urn);
+            })
+            .then(function (response) {
+                if (response.statusCode === 201) {
+                    grunt.log.writeln('Get token success!');
+                    return Promise.resolve(response.body.identityToken);
+                }else{
+                    grunt.log.error(response.body);
+                    throw ('Unexpected response: ' + response.statusCode);
                 }
-            }]);
+            })
+            .then(function (token) {
+                uploadOptions.token = token;
+                return upload(uploadOptions);
+            })
+            .then(function (urn) { // open in browser
+                if (gruntOptions.openInBrowserAfterUpload) {
+                    npmOpen(gruntOptions.serviceAddress + '/product/marketplace/widgets/config/' + urn);
+                }
+            })
+            .catch(function (error) {
+                grunt.log.error(error.toString());
+                done(false);
+            });
     });
 };
